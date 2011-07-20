040b011780266aba8432e9477c50e9f436b1e684
Daren Serra Dias Warburton <daren.sdw@gmail.com>
2011-07-21 00:20:38 +0100

    updated calendar images

--- app/helpers/availability_helper.rb
+++ app/helpers/availability_helper.rb
@@ -1,6 +1,47 @@
 module AvailabilityHelper
   
-  def calendar_availability(booked_dates, checkin_dates, checkout_dates, year, month, week_start)
+  def booked_days(bookings)
+    @year = Time.now.year
+		@month = Time.now.month
+		@month_sdate = ("#{@year}/#{@month}/01").to_date
+		@booked = Array.new
+		count = 0
+		bookings.each do |b|
+		  cur_date = b.start_at.to_date
+			end_date = b.end_at.to_date
+			until cur_date > end_date
+				@booked[count] = cur_date.to_date
+				count += 1
+				cur_date += 1
+			end
+		end
+		booked_days = @booked
+  end
+  
+  def checkin_days(bookings)
+		@checkin = Array.new
+		count = 0
+  	bookings.each do |b|
+			@checkin[count] = b.start_at.to_date
+			count += 1
+		end
+		checkin_days = @checkin
+  end
+
+  def checkout_days(bookings)
+		@checkout = Array.new
+		count = 0
+  	bookings.each do |b|
+			@checkout[count] = b.end_at.to_date
+			count += 1
+		end
+		checkout_days = @checkout
+  end
+  
+  def calendar_availability(booked, year, month, week_start)
+    booked_dates = booked_days(booked)
+    checkin_dates = checkin_days(booked)
+    checkout_dates = checkout_days(booked)
     raw calendar(:year => year, :month => month, :first_day_of_week => week_start) { |d|
     	booked_days = booked_dates
     	checkin_days = checkin_dates
