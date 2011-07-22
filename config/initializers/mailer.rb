ae4c17b273aa5835ae653db7883a6fdda38b3a9f
Daren Serra Dias Warburton <daren.sdw@gmail.com>
2011-07-22 18:31:57 +0100

    email updates

---
+++ config/initializers/mailer.rb
@@ -0,0 +1,11 @@
+ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {  
+  :address              => "smtp.bizmail.yahoo.com",  
+  :port                 => 587,
+  :domain               => "beefandfrog.com",  
+  :user_name            => "beefandfrog@yahoo.com",  
+  :password             => "password",
+  :authentication       => "plain",  
+  :enable_starttls_auto => true  
+}
